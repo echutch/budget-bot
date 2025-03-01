1b95c89b8f1d9fe4cd5dc39fba0cbc54bb55212b
echutch <echutch429@gmail.com>
2025-03-01 16:10:09 -0600

    Set up user model

---
+++ backend/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    bank_name = models.CharField(max_length=255, blank=True, null=True)
+    twilio_access_token = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
