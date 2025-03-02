fc405a3ff0967d62fd434fe50a0e41611f35b21e
Nyssa Aftab <nyssaa2@illinois.edu>
2025-03-01 18:33:12 -0600

    set up views

--- backend/email_handler/views.py
+++ backend/email_handler/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from twilio.twiml.messaging_response import MessagingResponse
+
+def sms_reply(request):
+    """Respond to incoming SMS with a simple text message."""
+    # Create a Twilio response
+    resp = MessagingResponse()
+
+    # Add a message to the response
+    resp.message("The Robots are coming! Head for the hills!")
+
+    # Return the TwiML response as an HTTP response
+    return HttpResponse(str(resp), content_type='application/xml')
+
